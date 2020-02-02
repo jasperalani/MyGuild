d792a8b08fdb0baf25b99dbcb4b893bc09337314
Jasper Alani <jasperalani@gmail.com>
2020-02-02 23:27:52 +0000

    Version 2 mostly style changes

--- src/index.php
+++ src/index.php
@@ -3,18 +3,20 @@ global $object, $assoc;
 $object = json_decode( file_get_contents( '../guilds.txt' ) );
 $assoc  = json_decode( file_get_contents( '../guilds.txt' ), true );
 
-function listGuilds( $e = 0, $order = 0 ) {
+function listGuilds( $e = 0, $page = 1, $order = 0 ) {
 	global $assoc;
-	if ( $order ) {
-		$assoc['guilds'] = array_reverse( $assoc['guilds'] );
-	}
+	$assoc['guilds'] = $order ? array_reverse( $assoc['guilds'] ) : $assoc['guilds'];
 	if ( $e > 0 ) {
-		foreach ( $assoc['guilds'] as $key => $guild ) {
+		$array = $assoc['guilds'];
+		$array = array_slice($array, $page*($e-1), $e);
+		echo '<div id="guild-list">';
+		foreach ( $array as $key => $guild ) {
 			echo "<span class='guild'><a href='?search=$guild'>$guild</a></span>";
 			if ( $key > $e ) {
 				break;
 			}
 		}
+		echo '</div>';
 	} else {
 		foreach ( $assoc['guilds'] as $guild ) {
 			echo "<span class='guild'><a href='?search=$guild'>$guild</a></span>";
@@ -40,6 +42,7 @@ function displayGuildInfo( $name ) {
 		return 0;
 	}
 
+	$name = urlencode($name);
 	$url  = 'https://api.wynncraft.com/public_api.php?action=guildStats&command=' . $name;
 	$curl = curl_init();
 	curl_setopt( $curl, CURLOPT_URL, $url );
@@ -57,46 +60,30 @@ function displayGuildInfo( $name ) {
 	?>
 
     <div id="search-results">
-        <h2><?= $res['name'] ?></h2>
-        <h3><?= $res['prefix'] ?></h3>
-        <p>current level: <?= $res['level'] ?></p>
-        <p>xp to next level: <?= $res['xp'] ?></p>
-        <p>territories owned: <?= $res['territories'] ?></p>
-        <p>created in <?= ( $monthName . " " . $year ) ?></p>
+        <div id="ginfo">
+	        <h2><?= $res['name'] ?></h2>
+	        <h3><?= $res['prefix'] ?></h3>
+	        <p>current level: <?= $res['level'] ?></p>
+	        <p>xp to next level: <?= $res['xp'] ?></p>
+	        <p>territories owned: <?= $res['territories'] ?></p>
+	        <p>created in <?= ( $monthName . " " . $year ) ?></p>
+        </div>
         <div id="gmembers">
-			<?php
-			exec( 'rm -R faces' );
-			exec( 'mkdir faces' );
-
-			foreach ( $res['members'] as $key => $member ) {
-				smallProfile( $member );
-			}
-
-			?>
+			<?php createOrderedProfiles($res); ?>
         </div>
     </div>
 	<?php
-
 
 	return 1;
 }
 
 function smallProfile( $member ) {
-
+	$img_src = 'https://minotar.net/avatar/' . $member['name'] . '/50.png';
 	$new_date = date( 'jS F Y', strtotime( $member['joined'] ) );
 	$small    = date( 'h:i:s', strtotime( $member['joined'] ) );
-
-	$curl = curl_init();
-	curl_setopt( $curl, CURLOPT_URL, 'https://crafatar.com/avatars/' . $member['uuid'] . '?size=50' );
-	curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-	$res = curl_exec( $curl );
-	curl_close( $curl );
-	file_put_contents( 'faces/' . $member['name'] . '.png', $res );
-
 	?>
-
     <div class="profile">
-        <img src="faces/<?= $member['name'] ?>.png">
+        <img src="<?= $img_src ?>">
         <div class="namerank">
             <span class="name"><?= $member['name'] ?></span><br>
             <small class="rank <?= $member['rank'] ?>"><i><?= strtolower( $member['rank'] ) ?></i></small><br><br>
@@ -109,6 +96,26 @@ function smallProfile( $member ) {
     </div>
 
 	<?php
+    return $member['rank'];
+}
+
+function createOrderedProfiles($res){
+	foreach ( $res['members'] as $key => $member ) {
+		$ranks[strtolower($member['rank'])][] = $key;
+	}
+	smallProfile($res['members'][$ranks['owner'][0]]);
+	foreach($ranks['chief'] as $chiefs){
+		smallProfile($res['members'][$chiefs]);
+	}
+	foreach($ranks['captain'] as $captains){
+		smallProfile($res['members'][$captains]);
+	}
+	foreach($ranks['recruiter'] as $recruiters){
+		smallProfile($res['members'][$recruiters]);
+	}
+	foreach($ranks['recruit'] as $recruits){
+		smallProfile($res['members'][$recruits]);
+	}
 }
 
 if ( isset( $_GET['search'] ) ) {
@@ -143,8 +150,29 @@ if ( isset( $_GET['search'] ) ) {
 <?php
 
 displaySearch();
-listGuilds( 400, 1 );
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+listGuilds( 400, $page, 0 );
+
+if(isset($_GET['pgback'])){
+	if(isset($_GET['page'])){
+
+		if($_GET['page'] < 1){
+			$_GET['page'] = 1;
+		}else{
+			$_GET['page'] = intval($_GET['page']) - 1;
+		}
+	}else{
+		var_dump($_GET);exit;
+		$_GET['page'] = 1;
+	}
+}
 
 ?>
+<form method="get" action="">
+	<input type="submit" name="pgback" value="Previous page">
+</form>
+<form method="get" action="">
+	<input type="submit" name="pgforward" value="Next page">
+</form>
 </body>
 </html>
